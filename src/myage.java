782dd90639303dd2a6a00b80ed2f7bdadc662287
ilabstaff <ilabstaff@172.16.116.67>
2019-02-27 17:42:04 -0800

    my age!

---
+++ src/myage.java
@@ -0,0 +1,11 @@
+
+public class myage {
+public static void main(String[] args) {
+ 
+	for(int i = 2002; i<2019; i++) {
+		System.out.println("in " + i + " i was " +(i - 2002) + (" years old! ") );
+	}
+}
+
+}
+
